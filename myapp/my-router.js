41e0a36610a158eedc038941d1f8d9629f6c34a4
cefariron <adidaswww17@gmail.com>
2024-03-28 23:24:46 +0200

    1

--- myapp/my-router.js
+++ myapp/my-router.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Визначимо домашній роутер
-router.get('/', (req, res) => {
-    res.send('Це головний роутер');
+router.get("/", (req, res) => {
+  res.send("Це головний роутер");
 });
 
 // Визначимо роутер about
-router.get('/about', (req, res) => {
-    res.send('About');
-    console.log('About');
+router.get("/about", (req, res) => {
+  res.send("About");
+  console.log("About");
 });
 
 module.exports = router;
